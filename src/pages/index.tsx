fc1e03fb72609c39f3ee5cd1f54e62f94b828002
Jonathan Bueno <jfb1601@hotmail.com>
2022-02-13 18:08:30 +0000

    Base styles for Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>início | ig.news</Head>
-      <h1>Hello World</h1>
+      <main className={`container ${styles.contentContainer}`}>
+        <section className={styles.hero}>
+          <span className={styles.hero__salutation}>👏 Hey, welcome</span>
+          <h1 className={styles.hero__title}>
+            News about the{' '}
+            <span className={styles['hero__title--highlight']}>React</span>{' '}
+            world.
+          </h1>
+          <p className={styles.hero__description}>
+            Get access to all the publications <br />
+            <span className={styles['hero__description--highlight']}>
+              for $9.90 month
+            </span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src='/images/avatar.svg' alt='Welcome' />
+      </main>
     </>
   );
 }
