e80527b1071bc53e42267cdacd9508f59b731f2d
Jonathan Bueno <jfb1601@hotmail.com>
2022-02-16 22:48:55 +0000

    Set-up of Stripe SDK. First SSR request for product amount on Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>início | ig.news</Head>
@@ -19,11 +28,11 @@ export default function Home() {
           <p className={styles.hero__description}>
             Get access to all the publications <br />
             <span className={styles['hero__description--highlight']}>
-              for $9.90 month
+              for {product.amount} month
             </span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src='/images/avatar.svg' alt='Welcome' />
@@ -31,3 +40,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KTaRADFg0J5T9ZOzN3vi7y9');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100), // work with cents
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
